e1f87f41c735c6e2db29c170ef71c7395b381d21
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-23 22:06:24 +0200

    add forEach function

--- js/products.js
+++ js/products.js
@@ -15,12 +15,48 @@ const products = [
   },
 ];
 
-// productContainer.innerHTML = `<h3>${product.name}</h3>
-//                                 <p>${product.price}</p>
-//                                 <p>${product.soldOut}</p>`
+const productContainer = document.querySelector(".container"); // Select element/place of display.
 
-const productContainer = document.querySelector(".container");
+// ForEach function
+//-------------------------------------------------------------------
+// products.forEach((product) => {
+//   const productDiv = document.createElement("div");
+//   productDiv.classList.add("itemcontainer");
+
+//   productDiv.innerHTML = `
+//       <h3>${product.name}</h3>
+//       <p>Price: ${product.price || "Not available"}</p>
+//       <p>Sold Out: ${product.soldOut ? "Yes" : "No"}</p>
+//   `;
+//   productContainer.appendChild(productDiv);
+// });
+//-------------------------------------------------------------------
+
+// or
+
+// For loop
+//-------------------------------------------------------------------
+
+// // let html = "";
 
 for (let i = 0; i < products.length; i++) {
-  products[i].name = `<h3>${product.name}</h3>`;
+  const productDiv = document.createElement("div"); // Create new div element for each product.
+
+  productDiv.classList.add("itemcontainer"); // Create a html class for each new html element.
+
+  productContainer.appendChild(productDiv); // Add new element under selected html element.
+
+  let itemPrice = "Unavailable";
+
+  if (products[i].price) {
+    itemPrice = products[i].price;
+  }
+
+  productDiv.innerHTML = `
+  <h3>${products[i].name}</h3>
+  <p>${products[i].price}</p>
+  <p>${products[i].soldOut}</p>   
+  `;
 }
+
+//-------------------------------------------------------------------
