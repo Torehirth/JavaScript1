86fbe157db67f55c0ec55bb4534e57c19ff9d317
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-23 09:45:50 +0200

    add for loop

--- js/products.js
+++ js/products.js
@@ -1,1 +1,26 @@
-const someThing = 0;
+const products = [
+  {
+    name: "Fancy Product",
+    price: 40,
+    soldOut: true,
+  },
+  {
+    name: "Another Product",
+    soldOut: false,
+  },
+  {
+    name: "Cheap Product",
+    price: 10,
+    soldOut: true,
+  },
+];
+
+// productContainer.innerHTML = `<h3>${product.name}</h3>
+//                                 <p>${product.price}</p>
+//                                 <p>${product.soldOut}</p>`
+
+const productContainer = document.querySelector(".container");
+
+for (let i = 0; i < products.length; i++) {
+  products[i].name = `<h3>${product.name}</h3>`;
+}
