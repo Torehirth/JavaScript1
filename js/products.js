34521c1b75cd423c9110fecf0ef90270f6e6fa5b
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-24 20:51:08 +0200

    did first section of lesson task 3

--- js/products.js
+++ js/products.js
@@ -37,8 +37,6 @@ const productContainer = document.querySelector(".container"); // Select element
 // For loop
 //-------------------------------------------------------------------
 
-// // let html = "";
-
 for (let i = 0; i < products.length; i++) {
   const productDiv = document.createElement("div"); // Create new div element for each product.
 
@@ -46,14 +44,21 @@ for (let i = 0; i < products.length; i++) {
 
   productContainer.appendChild(productDiv); // Add new element under selected html element.
 
-  let itemPrice = "Unavailable";
+  if (!products[i].price) {
+    products[i].price = "Unavailable";
+  }
 
-  if (products[i].price) {
-    itemPrice = products[i].price;
+  let colour = "green";
+
+  if (products[i].soldOut) {
+    colour = "red";
+    products[i].soldOut = "Not in stock";
+  } else {
+    products[i].soldOut = "In stock";
   }
 
   productDiv.innerHTML = `
-  <h3>${products[i].name}</h3>
+  <h3 style="color: ${colour}">${products[i].name}</h3>
   <p>${products[i].price}</p>
   <p>${products[i].soldOut}</p>   
   `;
