19488720c025e9c1368cadfc14c18151155b2747
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-24 22:37:04 +0200

    did second section of lesson 3

--- js/product.js
+++ js/product.js
@@ -6,5 +6,28 @@ const product = {
   onSale: true,
 };
 
+const container = document.querySelector(".productcontainer");
 
+let html = "";
 
+if (!product.price) {
+  product.price = "Not available";
+}
+
+let saleUnderline = "none";
+let saleAnswer = "no";
+
+if (product.onSale) {
+  saleUnderline = "underline";
+  saleAnswer = "yes";
+} else {
+  saleAnswer = "no";
+}
+
+html += `
+<h3 style="text-decoration: ${saleUnderline}">${product.name}</h3>
+<p>Price: ${product.price}$</p>
+<p>On sale: ${saleAnswer}</p>
+`;
+
+container.innerHTML = html;
