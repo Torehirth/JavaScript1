be76cdc9c8a7d309def36937129bac4984055457
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-26 20:12:30 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Module 1 Lesson 4
 
+const sports = ["golf", "football", "cricket"];
+
+const games = [
+  {
+    name: "Starcraft 2",
+    released: "2010",
+  },
+  {
+    name: "Age of Empires II",
+  },
+  {
+    name: "Cyberpunk 2077",
+    released: 2020,
+  },
+];
+
 // Question 1
 
 // Question 2
