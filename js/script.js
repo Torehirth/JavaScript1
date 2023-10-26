a36ccde904a1739e8f246162598397f2e6140a0b
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-26 22:38:56 +0200

    did three first questions

--- js/script.js
+++ js/script.js
@@ -16,13 +16,38 @@ const games = [
   },
 ];
 
-
-
 // Question 1
+
+function printSeason(season) {
+  console.log(season);
+}
+
+printSeason("summer");
 
 // Question 2
 
+function printNumbers(firstNumber, secondNumber) {
+  console.log(`
+  first number: ${firstNumber} 
+  second number: ${secondNumber}
+  `);
+}
+
+printNumbers(2, 5);
+
 // Question 3
+
+function addNumbers(firstNumber, secondNumber, thirdNumber) {
+  const result = firstNumber + secondNumber + thirdNumber;
+
+  return result;
+}
+
+const total = addNumbers(2, 4, 4);
+
+const resultContainer = document.querySelector(".total");
+
+resultContainer.innerHTML = total;
 
 // Question 4
 
