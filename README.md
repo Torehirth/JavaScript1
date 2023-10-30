6825cf118ed5167597b692ecb8cd9768978051b6
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-30 20:28:00 +0100

    delete code from previous branch and added md and css from module 2 lesson 2 repo

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
 # JavaScript 1 - Module 2
 
-## Lesson Task 1 Questions
+## Lesson Task 2 Question
 
-### Question 1
+Add a click event to the button that disables the button and starts a counter.
 
-Create a function called `receivingFunction` which accepts one argument. Inside this function check if the argument is a function, and if it is, call the function.
+Count to 7 and update `span.count` with the value of the count.
 
-Create another function called `callbackFunction` which logs the string: "I am a callback function".
-
-Call `receivingFunction` and pass `callbackFunction` into it as an argument.
-
-### Question 2
-
-Convert the following `for` loop to a `forEach` loop.
-
-```js
-const prizes = ["Bag of bananas", "Box of frogs", "Car of clowns"];
-
-for (let i = 0; i < prizes.length; i++) {
-    console.log(`Prize ${i + 1}: ${prizes[i]}`);
-}
-```
-
-### Question 3
-
-Write code that waits 5 seconds before logging the string: `I waited 5 seconds before executing`. Store the amount of seconds (5 initially) in a variable so it can be easily changed.
-
-### Question 4
-
-Write code that logs the numbers 1 to 4 every 1.5 seconds and then stops.
+Once 7 is reached stop the count and enable the button.
--- js/script.js
+++ js/script.js
@@ -1,78 +1,11 @@
-// Module 2 Lesson 1
+// Module 2 Lesson 2
 
 // Question 1
 
-function receivingFunction(someArgument) {
-  // console.log("this is a function");
-
-  if (typeof someArgument === "function") {
-    someArgument();
-  } else {
-    console.log("Argument is not a function");
-  }
-}
-
-function callBackFunction() {
-  console.log("I am a callback function");
-}
-
-receivingFunction(callBackFunction);
-
 // Question 2
-
-const prizes = ["Bag of bananas", "Box of frogs", "Car of clowns"];
-
-// for (let i = 0; i < prizes.length; i++) {
-//   console.log(`Prize ${i + 1}: ${prizes[i]}`);
-// }
-
-prizes.forEach(function (item, index) {
-  console.log(`Prize ${index + 1}: ${item}`);
-});
 
 // Question 3
 
-function timeOut() {
-  console.log("I waited 5 seconds before executing");
-}
-
-const waitingTime = 5000; // 5 seconds
-
-setTimeout(timeOut, waitingTime); // using the timeOut function as a parameter as well as the waitingTime variabel.
-
 // Question 4
 
-let number = 1;
-const intervalTime = 1500;
-
-function numberCount() {
-  console.log(number++); // log number (starting from 1) and increase value by 1 each time.
-
-  if (number > 4) {
-    clearInterval(interval);
-    // Stop interval when number is bigger than 4 (number 5 doesn't show)
-  }
-}
-
-const interval = setInterval(numberCount, intervalTime);
-
-// or
-
-// const interval = 1500;
-// let count = 0;
-// const limit = 4;
-
-// function logNumber() {
-//   // increase count's value by 1
-//   count++;
-
-//   // log count's current value
-//   console.log(count);
-
-//   // check whether to clear the setInterval
-//   if (count === limit) {
-//     clearInterval(intervalId);
-//   }
-// }
-
-// const intervalId = setInterval(logNumber, interval);
+// Question 5
