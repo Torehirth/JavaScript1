100433e455b4acf66878c3f9dfccbdb7338780bb
Tore Mæland Hirth <torehirth@gmail.com>
2023-10-30 20:22:53 +0100

    did question 3 and 4

--- js/script.js
+++ js/script.js
@@ -32,4 +32,47 @@ prizes.forEach(function (item, index) {
 
 // Question 3
 
+function timeOut() {
+  console.log("I waited 5 seconds before executing");
+}
+
+const waitingTime = 5000; // 5 seconds
+
+setTimeout(timeOut, waitingTime); // using the timeOut function as a parameter as well as the waitingTime variabel.
+
 // Question 4
+
+let number = 1;
+const intervalTime = 1500;
+
+function numberCount() {
+  console.log(number++); // log number (starting from 1) and increase value by 1 each time.
+
+  if (number > 4) {
+    clearInterval(interval);
+    // Stop interval when number is bigger than 4 (number 5 doesn't show)
+  }
+}
+
+const interval = setInterval(numberCount, intervalTime);
+
+// or
+
+// const interval = 1500;
+// let count = 0;
+// const limit = 4;
+
+// function logNumber() {
+//   // increase count's value by 1
+//   count++;
+
+//   // log count's current value
+//   console.log(count);
+
+//   // check whether to clear the setInterval
+//   if (count === limit) {
+//     clearInterval(intervalId);
+//   }
+// }
+
+// const intervalId = setInterval(logNumber, interval);
