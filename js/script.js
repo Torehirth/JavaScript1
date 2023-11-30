21d74c893d9168dba15a337e7fbe5ad40835da8d
Tore Mæland Hirth <torehirth@gmail.com>
2023-11-30 20:40:20 +0100

    add questions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 // Module 3 lesson 2
 
 // Question 1:
+// Give the below function's name parameter a default value of "John"
+
+function greet(name) {
+  return `Hello ${name}!`;
+}
 
 // Question 2:
+//Create a reusable function that accepts two parameters, then returns a dynamic HTML <div> element.
+// The <div> it creates should insert one of the parameters as a class, and the other as the inner value. Similar to the below HTML:
+
+{/* <div class="class">value</div> */}
 
 // Question 3:
+// Convert the below API call to use a try-catch-finally statement.
 
-// Question 4:
+async function getFact() {
+  const response = await fetch("https://api.noroff.dev/api/v1/cat-facts");
+  const results = await response.json();
+  console.log(results[8].text);
+}
 
-// Question 5:
+
