4820262ddb07ce2b12a905f70eaf84630abf543c
Tore Mæland Hirth <torehirth@gmail.com>
2023-12-03 13:05:44 +0100

    finished questions module 3 lesson 2

--- js/script.js
+++ js/script.js
@@ -18,9 +18,9 @@ console.log(greet());
 // --------Answer below------------
 
 // // resusable function that creates div element with classname and innervalue.
-// function divCreator(className, innerValue) {
-//   return `<div class="${className}">${innerValue}</div>`;
-// }
+function divCreator(className, innerValue) {
+  return `<div class="${className}">${innerValue}</div>`;
+}
 
 // Resusable function that changes the classname and innerHTML, with default values in case undefined.
 // function createMessage(type = "success", message = "no message") {
@@ -36,7 +36,7 @@ console.log(greet());
 
 // Or another way of creating this function below with arrow function syntax:
 
-const createMessage = (type = "success", innerText = "no message") => {
+const createMessage = (type = "warning", innerText = "no message") => {
   const className = document.querySelector(".message");
 
   className.classList.add(type);
@@ -50,8 +50,52 @@ createMessage("success", "correct value");
 // Question 3:
 // Convert the below API call to use a try-catch-finally statement.
 
-async function getFact() {
-  const response = await fetch("https://api.noroff.dev/api/v1/cat-facts");
-  const results = await response.json();
-  console.log(results[8].text);
-}
+// async function getFact() {
+//   const message = document.querySelector(".message");
+//   try {
+//     const response = await fetch("https://api.noroff.dev/api/v1/cat-facts");
+//     const results = await response.json();
+
+//     message.classList.remove("success", "error", "warning");
+//     message.classList.add("success");
+
+//     message.innerText = results[8].text;
+//     console.log(results[8].text);
+//   } catch (error) {
+//     // Error message if somethings goes wrong with the function
+//     console.log(error);
+//     message.innerText = "something went wrong";
+//     message.classList.remove("success", "error", "warning");
+//     message.classList.add("error");
+//   }
+// }
+
+// getFact();
+
+// --------------or in a cleaner way-------------------------
+
+const getMessageElement = document.querySelector(".message");
+
+const updateMessage = (text, messageType = "success") => {
+  const message = getMessageElement;
+  message.classList.remove("success", "error", "warning");
+  message.classList.add(messageType);
+  message.innerText = text;
+};
+
+const getFact = async () => {
+  try {
+    const response = await fetch("https://api.noroff.dev/api/v1/cat-facts");
+    const results = await response.json();
+
+    updateMessage(results[8].text);
+    console.log("Success:", results[8].text);
+  } catch (error) {
+    console.error("Error:", error);
+    updateMessage("something went wrong", "error");
+  } finally {
+    console.log("Finally: this will run whether there's an error or not!");
+  }
+};
+
+getFact();
