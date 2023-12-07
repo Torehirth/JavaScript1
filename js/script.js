e10f5b38606b94f0eb66e303566a5ab3760c83e6
Tore Mæland Hirth <torehirth@gmail.com>
2023-12-07 20:29:12 +0100

    deleted old code, added questions

--- js/script.js
+++ js/script.js
@@ -1,85 +1,15 @@
-// Module 3 lesson 3
+// Module 3 lesson 4
 
 // Question 1:
 
-// Convert the below functions to one-line arrow functions.
+// Add headers to the below API call.
 
-// function greet(name) {
-//   return `Hello ${name}!`;
-// }
+// You can choose whether to create a variable for the headers object seperately from the call and pass the variable in as the second parameter, or pass the headers object in directly.
 
-// function add(a, b) {
-//   return a + b;
-// }
+const API_URL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat";
 
-// answer:
-
-const greet = (name) => {
-  `hello ${name}!`;
-};
-
-const add = (a, b) => {
-  a + b;
-};
-
-// Question 2:
-
-// Convert the below code to use the then/catch syntax instead.
-
-// async function getCatFacts() {
-//   try {
-//     const response = await fetch("https://api.noroff.dev/api/v1/cat-fact");
-//     const results = await response.json();
-//     console.log(results.length);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// getCatFacts();
-
-// Answer:
-
-fetch("https://api.noroff.dev/api/v1/cat-fact")
-  .then(function (response) {
-    console.log(response);
-
-    return response.json();
-  })
-  .then(function (results) {
-    console.log(results);
-    console.log(results.length);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-// we can also enclose the code in a function and then call it
-// this will have the same effect but means the code is reusable and can be called again from somewhere else in the code
-
-function getCatFacts() {
-  fetch("https://api.noroff.dev/api/v1/cat-facts")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (results) {
-      console.log(results.length);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+async function callUrbanDictionary() {
+  const response = await fetch(API_URL);
+  const results = await response.json();
+  console.log(results);
 }
-
-getCatFacts();
-
-// with arrow functions
-function getCatFacts2() {
-  fetch("https://api.noroff.dev/api/v1/cat-facts")
-    .then((response) => response.json())
-    .then((results) => {
-      console.log(results.length);
-    })
-    .catch((error) => console.log(error));
-}
-
-getCatFacts2();
