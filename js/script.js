a4ad41db9b8f781fb300f23595a23663b800b7e7
Tore Mæland Hirth <torehirth@gmail.com>
2023-12-27 19:54:07 +0100

    make api call

--- js/script.js
+++ js/script.js
@@ -10,4 +10,17 @@
 
 // There will be a maximum of 15 results displayed if no teams' names begin with "C", and less than 15 displayed if there are teams whose names begin with "C".
 
+const resultsContainer = document.querySelector("#container h1");
+
 const URL = "https://raw.githubusercontent.com/bttmly/nba/master/data/teams.json";
+
+async function makeApiCall() {
+  try {
+    const response = await fetch(URL);
+    const results = response.json();
+    
+  } catch (error) {
+    console.log(error);
+    resultsContainer.innerHTML = message("error", error);
+  }
+}
