e0946700ad2a423bdde09c9e886e4c815392939f
Tore Mæland Hirth <torehirth@gmail.com>
2024-01-05 13:34:56 +0100

    finished question and API call

--- js/script.js
+++ js/script.js
@@ -16,24 +16,47 @@ const resultsContainer = document.querySelector(".container");
 
 const URL = "https://raw.githubusercontent.com/bttmly/nba/master/data/teams.json";
 
-async function makeApiCall() {
+async function fetchTeams() {
   try {
     const response = await fetch(URL);
     const results = await response.json();
 
+    // logs the results og the API call.
     console.log(results);
 
-    results.forEach((element) => {
+    resultsContainer.innerHTML = ""; // to end loader/spinner
+
+    for (let i = 0; i < results.length; i++) {
+      // We only want to display 15 teams.
+      if (i === 15) {
+        break;
+      }
+
+      // If the teamname starts with the letter C or c, the loop continues (doesn't show teams with letter C or c).
+      if (results[i].teamName.toLowerCase().startsWith("c")) {
+        continue;
+      }
+
+      // Alternative to startsWith method is the charAt method:
+      // // If the teamname starts with the letter C or c, the loop continues (doesn't show teams with letter C or c).
+      // // ther zero in charAt(0) represents the first letter.
+      // if (results[i].teamName.charAt(0).toUpperCase() === "C") {
+      //   continue;
+      // }
+
       resultsContainer.innerHTML += `
-       <div class="card">
-       <h2>${element.teamName}</h2>
-       </div>
-       `;
-    });
+                                    <div class="card">
+                                    <h2>${results[i].teamName}</h2>
+                                    <h3>Location: ${results[i].location}</h3>
+                                    <h5>Team Id: ${results[i].teamId}</h5>
+                                    </div>
+                                    `;
+
+      console.log(results[i].teamName);
+    }
   } catch (error) {
     console.log(error);
-    resultsContainer.innerText = message("something went wrong", error);
+    resultsContainer.innerHTML = message("error", error);
   }
 }
-
-makeApiCall();
+fetchTeams();
